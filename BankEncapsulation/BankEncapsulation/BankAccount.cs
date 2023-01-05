a75ae3dfdd3cae6e1765cc1b0c097eb393d74cf7
Kyle Jenne <strongagent1@gmail.com>
2023-01-05 12:19:58 -0500

    exercise for encapsulation bank demo -KJ

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+        private double balance = 200;
+        public void Deposit(double depositAmount)
+        {
+            
+
+            balance += depositAmount;
+            
+        }
+        public double GetBalance()
+        {
+            return balance;
+            
+        }
+    }
+}
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstAccount = new BankAccount();
+
+            Console.WriteLine("Hello, Please tell me if you would like to see your balance? y/n?");
+            var userBalanceResponse = Console.ReadLine().ToLower();
+            
+
+            if (userBalanceResponse == "y")
+            {
+                Console.WriteLine($"{firstAccount.GetBalance()}");
+
+
+            }
+         
+            else
+            {
+                Console.WriteLine("Would you like to make a deposit? y/n?");
+                var userDepositResponse = Console.ReadLine().ToLower();
+
+                if (userDepositResponse == "y")
+                {
+                    Console.WriteLine($"How much would you like to deposit?");
+                    var depositAmount = double.Parse(Console.ReadLine());
+                    firstAccount.Deposit(depositAmount);
+                    
+                    Console.WriteLine($"Thank you, your balance is now {firstAccount.GetBalance()}.");
+                    
+
+                }
+                else
+                {
+                    
+                    {
+                        if(userBalanceResponse == "n")
+                        {
+                            Console.WriteLine($"Thank you for visiting ABC Financial Services and have a great day!");
+                        }
+                        else
+                        {
+                            
+                        }
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
